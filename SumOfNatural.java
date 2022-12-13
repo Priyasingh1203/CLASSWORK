e728d9dbb88e47b4ce2a7b31a0c018379d506919
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2022-12-13 12:49:30 -0500

    Add files via upload

---
+++ SumOfNatural.java
@@ -0,0 +1,14 @@
+
+public class SumOfNatural
+{
+  public static void main(String[] args)
+  {
+     int i, sum=0;
+     
+     for(i=1; i<=10; i++)
+     {
+        sum = sum +i;
+     }
+     System.out.println("SUM OF NATURAL NUMBER IS:" +sum);
+  }
+}   
