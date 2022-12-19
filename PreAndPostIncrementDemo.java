1d124e837daa5f75223f126ed1025662eaa91bb4
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2022-12-19 14:00:46 +0530

    Add files via upload

---
+++ PreAndPostIncrementDemo.java
@@ -0,0 +1,15 @@
+package basicprogram;
+
+public class PreAndPostIncrementDemo {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=x++;
+		//int y=++x;
+		
+		
+		System.out.println(y);
+		System.out.println(x);		
+	}
+
+}
