28b1f28c40a7b24f7ad57d6c1124141e14106ddf
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2022-12-19 13:58:26 +0530

    Add files via upload

---
+++ Cal.java
@@ -0,0 +1,12 @@
+package calculation;
+public class Cal1{
+	public void factorial(int n)
+	{
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("factorial of a number is" +fact);
+	}
+}
