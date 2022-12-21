e8a5ec1b149e5a392807d7aa8740998c90134f04
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2022-12-21 23:21:09 +0530

    Add files via upload

---
+++ ConstructorOverloading.java
@@ -0,0 +1,46 @@
+package oops;
+class Students
+{
+	int id;
+	String name;
+	int age;
+	long phno;
+Students(int id,String name)
+{
+	this.id=id;
+	this.name=name;
+	}
+Students (int id,String name,int age)
+{
+	
+	this.id=id;
+	this.name=name;
+	this.age=age;
+	}
+Students (int id,String name,int age,long phno)
+{
+	
+	this.id=id;
+	this.name=name;
+	this.age=age;
+	this.phno=phno;
+	}
+public void display()
+{
+	System.out.println("Id: "+id+"\n"+"Name: "+name+"\n"+"Age: "+age+"\n"+"Phone: "+phno);
+	}
+}
+public class ConstructorOverloading {
+
+	public static void main(String[] args) {
+		Students s1=new Students(101,"Priya");
+		Students s2=new Students(101,"Priya",12);
+		Students s3=new Students(101,"Priya",12,875462120);
+		s1.display();
+		s2.display();
+		s3.display();
+		
+
+	}
+
+}
