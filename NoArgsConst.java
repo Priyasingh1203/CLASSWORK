91b4656ac15b0289b864fc342dd73ce383fbf027
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2022-12-21 23:21:43 +0530

    Add files via upload

---
+++ NoArgsConst.java
@@ -0,0 +1,16 @@
+package oops;
+class NoagrsConstructor
+{
+	public NoagrsConstructor()
+	{
+		System.out.println("This is no argument constructor");
+	}
+	}
+public class NoArgsConst {
+
+	public static void main(String[] args) {
+		NoagrsConstructor nac=new NoagrsConstructor();
+
+	}
+
+}
